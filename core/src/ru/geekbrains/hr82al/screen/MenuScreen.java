700879c7d4b3a774f5c169fb35386ad68fca3b11
hr82al@gmail.com <hr82al@gmail.com>
2018-10-26 12:38:33 +0300

    Added new classes.

--- core/src/ru/geekbrains/hr82al/screen/MenuScreen.java
+++ core/src/ru/geekbrains/hr82al/screen/MenuScreen.java
@@ -12,12 +12,6 @@ public class MenuScreen extends Base2DScreen {
     private SpriteBatch batch;
     private Texture img;
     private Texture movedImg;
-    private Vector2 position;
-    private Vector2 velocity;
-    private Vector2 direction;
-    private Vector2 destination;
-    private int steps;
-    private Vector2 buf = new Vector2();
 
     @Override
     public void show() {
@@ -25,20 +19,6 @@ public class MenuScreen extends Base2DScreen {
         batch = new SpriteBatch();
         img = new Texture("background.jpg");
         movedImg = new Texture("badlogic.jpg");
-        position = new Vector2(0f, 0f);
-        velocity = new Vector2(0.7f, 0.9f);
-        direction = velocity.cpy();
-        destination = new Vector2(0f, 0f);
-        setDirection(position, destination);
-    }
-
-    private void setDirection(Vector2 position, Vector2 destination) {
-        buf.set(destination);
-        buf.sub(position);
-        steps = (int)(buf.len() / velocity.len());
-        buf.nor();
-        direction.set(velocity);
-        direction.scl(buf);
     }
 
     @Override
@@ -46,26 +26,16 @@ public class MenuScreen extends Base2DScreen {
         super.render(delta);
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.getProjectionMatrix().idt();
         batch.begin();
-        batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.draw(movedImg, position.x, position.y);
-        batch.end();
-        moveToDestination();
+        batch.draw(img, -1f, -1f, 2f, 2f);
+        batch.draw(movedImg, 0,0, 1f, 1f);
+        batch.end();;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        destination.set((float)screenX, Gdx.graphics.getHeight() - (float)screenY);
-        setDirection(position, destination);
         return false;
-    }
-
-    private void moveToDestination() {
-        if (steps-- > 0) {
-            position.add(direction);
-        } else {
-            position.set(destination);
-        }
     }
 
     @Override
